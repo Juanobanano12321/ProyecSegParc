af0d2174e274f5dc56a06200038c04c26e820ea1
Juanobanano12321 <caudillo.villegas.juan.pablo1@gmail.com>
2022-10-12 07:48:33 -0500

    Primera version

---
+++ app/src/main/java/com/example/myslash/Register.java
@@ -0,0 +1,21 @@
+package com.example.myslash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void IniciarSesion (View v){
+        Intent intent = new Intent (Register.this, Login.class);
+        startActivity( intent );
+    }
+}
